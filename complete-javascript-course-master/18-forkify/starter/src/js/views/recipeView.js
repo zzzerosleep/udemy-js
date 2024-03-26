9a88cde5be84bbf6a570875860f7ce2c0b97b930
zzzerosleep <kennedykills@gmail.com>
2024-03-26 17:07:36 -0500

    updated fractional package to fracty

--- complete-javascript-course-master/18-forkify/starter/src/js/views/recipeView.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 import View from './View.js';
 
 class RecipeView extends View {
@@ -125,7 +125,7 @@ class RecipeView extends View {
        <use href="${icons}#icon-check"></use>
       </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? fracty(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
